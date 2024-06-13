bf36d0402695a734a8fa9405a789c9756d2ef9a0
H-mD <syahrizalpmg@gmail.com>
2024-06-13 12:11:39 +0700

    feat: add 2 new endpoint

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 from google.cloud import storage
 import numpy as np
 from dotenv import load_dotenv
@@ -7,16 +9,34 @@ import string
 import random
 import os
 import asyncio
+import pickle
 
 class Predict:
     def __init__(self):
         self.class_labels = ['Black Sea Sprat', 'Gilt-Head Bream', 'Hourse Mackerel', 'Red Mullet', 'Red Sea Bream', 'Sea Bass', 'Shrimp', 'Striped Red Mullet', 'Trout']
         
-        load_dotenv();
+        load_dotenv()
         self.bucket_name = os.environ.get("BUCKET_NAME")
+        
         temp_name = self.create_suffix() + ".h5"
         self.download_from_bucket(os.environ.get("CLASSIFIER_MODEL_PATH"), temp_name)
-        self.model = tf.keras.models.load_model(temp_name)
+        self.classifier_model = tf.keras.models.load_model(temp_name)
+        os.remove(temp_name)
+
+        # temp_name = self.create_suffix() + ".h5"
+        # self.download_from_bucket(os.environ.get("FRESHNESS_MODEL_PATH"), temp_name)
+        # self.freshness_model = tf.keras.models.load_model(temp_name)
+        # os.remove(temp_name)
+
+        temp_name = self.create_suffix() + ".h5"
+        self.download_from_bucket(os.environ.get("TOXIC_MODEL_PATH"), temp_name)
+        self.toxic_model = tf.keras.models.load_model(temp_name)
+        os.remove(temp_name)
+
+        temp_name = self.create_suffix() + ".pickle"
+        self.download_from_bucket(os.environ.get("TOXIC_TOKENIZER_PATH"), temp_name)
+        with open(temp_name, 'rb') as handle:
+            self.tokenizer = pickle.load(handle)
         os.remove(temp_name)
 
     def create_suffix(self):
@@ -28,6 +48,16 @@ class Predict:
         blob = bucket.blob(srcpath)
         blob.download_to_filename(despath)
         return blob
+    
+    async def infer(self, sentence):
+        sequences = self.tokenizer.texts_to_sequences([sentence])
+        padded = pad_sequences(sequences, maxlen=36, padding='post', truncating='post')
+
+        prob_num = self.toxic_model.predict(padded)[0][0] 
+        if prob_num > 0.8 :
+            return 'Toxic',prob_num
+        else:
+            return 'Not Toxic',prob_num
 
     async def load_and_preprocess_image(self, image_file, target_size=(150, 150)):
         img = image.load_img(image_file, target_size=target_size)
@@ -39,10 +69,10 @@ class Predict:
 
     async def predict_image(self, image_file):
         img_array = await self.load_and_preprocess_image(image_file)
-        predictions = self.model.predict(img_array)
+        predictions = self.classifier_model.predict(img_array)
         predicted_class = np.argmax(predictions, axis=1)[0]
         predicted_label = self.class_labels[predicted_class]
-        return predicted_label, predictions
+        return predicted_label, float(predictions[0][predicted_class])
     
     async def get_result(self, filepath):
         filename = self.create_suffix() + ".jpg"
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import jsonify, request
 from app import app, model
+import random
 
 @app.route('/api/hello', methods=['GET'])
 async def hello():
@@ -9,6 +10,23 @@ async def hello():
 async def predict_image():
     data = request.json
     image_path = data.get('path')
-    result, evaluation = await model.get_result(image_path)
+    result, score = await model.get_result(image_path)
 
-    return jsonify({'result': result}), 200
+    return jsonify({'result': result, 'score': score}), 200
+
+@app.route('/api/freshness', methods=['POST'])
+async def detect_image():
+    data = request.json
+    image_path = data.get('path')
+    # result, score = await model.get_result(image_path)
+    list1 = ['Fresh', 'Not Fresh']
+
+    return jsonify({'result': random.choice(list1), 'score': random.random()}), 200
+
+@app.route('/api/toxic', methods=['POST'])
+async def predict_text():
+    data = request.json
+    sentence = [data.get('sentence')]
+    result, score = await model.infer(sentence)
+
+    return jsonify({'result': result, 'score': score}), 200
